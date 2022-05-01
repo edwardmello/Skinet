e6b7660e03b93fde1a998c916d210d003b70c455
Edward Mello <edwardm@za.neolife.com>
2022-05-01 15:59:41 +0200

    End of Section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { of } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +7,9 @@ import { IPagination } from './models/pagination';
 })
 export class AppComponent implements OnInit{
   title = 'Skinet';
-  products: IProduct[];
   
-  constructor(private http: HttpClient){}
+  constructor(){}
 
   ngOnInit(): void {
-    //this.http.get('http://localhost:5255/api/products').subscribe((response:any) => {console.log(response)});
-    this.http.get('https://localhost:7255/api/products?pageSize=50').subscribe({
-      
-    next : (response:IPagination) =>  this.products =response.data,
-      error : (error) => console.log(error)
-    });
-    
   }
 }
